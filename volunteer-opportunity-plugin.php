8874153fd9e9db4fc65222c38d7670605b015e9c
Nevil Patel <npatel@fleetoperate.com>
2025-01-10 12:16:20 -0500

    FEAT: plugin details

---
+++ volunteer-opportunity-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @package Volunteer Opportunity
+ */
+/*
+Plugin Name: Volunteer Opportunity Plugin
+Description: This is the Volunteer Opportunity Plugin for managing volunteer details dynamically and filter as per their volunteering hours.
+Version: 0.1
+Author: Nevil Patel
+*/
